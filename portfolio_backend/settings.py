a3a8a30f39f40dc6cbd49c5227c146d34adb676c
Abu Naim <abunaim@Abus-Air-2.lan>
2024-06-19 14:29:14 +0600

    pyobj remove

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -27,9 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
-# CSRF_TRUSTED_ORIGINS = [
-    
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    'https://portfolio-backend-o1rq.onrender.com',
+]
 
 
 # Application definition
